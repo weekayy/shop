109ecfa58f5702991fae0efc26fa79c897f9950c
weekayy <1370822297@qq.com>
2020-01-04 14:14:32 +0800

    更改BaseController

--- src/main/java/com/ofilm/yk/controller/BaseController.java
+++ src/main/java/com/ofilm/yk/controller/BaseController.java
@@ -1,10 +1,14 @@
 package com.ofilm.yk.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BaseController {
+
+	private static final Logger log = LoggerFactory.getLogger(BaseController.class);
 
 	@RequestMapping("/wel")
 	public String wel() {
